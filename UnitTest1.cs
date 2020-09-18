538f9d48c53ac15ca1a3a31b3eb96f0bcb52ad11
Gokul-Shanmugam884 <71479427+Gokul-Shanmugam884@users.noreply.github.com>
2020-09-18 10:03:03 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using java.awt;
+using java.awt.datatransfer;
+using java.awt.@event;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace demo.opencart
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driver;
+        static components reuse;
+        static WebDriverWait wait;
+        [ClassInitialize]
+        public static void setup(TestContext context)
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            reuse = new components(driver);
+            wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            driver.Url = reuse.readConfig("url");
+        }
+
+
+        [ClassCleanup]
+        public static void clean()
+        {
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void TC01_Registration()
+        {
+            driver.FindElement(By.XPath("//span[@class='caret']")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Register')]")).Click();
+            Dictionary<string, string> details = reuse.GetRegDetails(5);
+            driver.FindElement(By.Id("input-firstname")).SendKeys(details["firstname"]);
+            driver.FindElement(By.Id("input-lastname")).SendKeys(details["lastname"]);
+            driver.FindElement(By.Id("input-email")).SendKeys(details["email"]);
+            driver.FindElement(By.Id("input-telephone")).SendKeys(details["phone"]);
+            driver.FindElement(By.Id("input-password")).SendKeys(details["password"]);
+            driver.FindElement(By.Id("input-confirm")).SendKeys(details["confirmpassword"]);
+            reuse.writeResult("Checkbox clicked : " + driver.FindElement(By.Name("agree")).Selected);
+            driver.FindElement(By.Name("agree")).Click();
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            reuse.writeResult("Register : " + driver.FindElement(By.XPath("//div[@id='content']/h1")).Text);
+            reuse.takeScreenshot("Registration");
+            reuse.logout();
+            reuse.writeResult("Logout : " + driver.FindElement(By.XPath("//div[@id='content']")).Text);
+        }
+
+        [TestMethod]
+        public void TC02_Login_and_Edit_Account_information()
+        {
+            Dictionary<string, string> details = reuse.GetLogDetails(2);
+            driver.FindElement(By.XPath("//span[@class='caret']")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Login')]")).Click();
+            driver.FindElement(By.Id("input-email")).SendKeys(details["email"]);
+            driver.FindElement(By.Id("input-password")).SendKeys(details["password"]);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            reuse.takeScreenshot("Login");
+            driver.FindElement(By.XPath("//a[contains(text(),'Edit your account')]")).Click();
+            driver.FindElement(By.Id("input-telephone")).Clear();
+            driver.FindElement(By.Id("input-telephone")).SendKeys("986532147");
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            reuse.takeScreenshot("Telephone update");
+            reuse.logout();
+            reuse.writeResult("Logout : " + driver.FindElement(By.XPath("//div[@id='content']")).Text);
+        }
+
+        [TestMethod]
+        public void TC03_Add_product_to_cart_and_make_payment()
+        {
+            Dictionary<string, string> details = reuse.GetLogDetails(2);
+            driver.FindElement(By.XPath("//span[@class='caret']")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Login')]")).Click();
+            driver.FindElement(By.Id("input-email")).SendKeys(details["email"]);
+            driver.FindElement(By.Id("input-password")).SendKeys(details["password"]);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            reuse.takeScreenshot("Login");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Components')])[1]")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Monitors')]")).Click();
+            reuse.takeScreenshot("Monitors");
+            string name = driver.FindElement(By.XPath("(//div[@class='product-thumb'])[1]/div[2]/div[1]/h4")).Text;
+            string prize = driver.FindElement(By.XPath("(//div[@class='product-thumb'])[1]/div[2]/div[1]/p[2]/span[1]")).Text;
+            reuse.writeOutput(name + " : " + prize);
+            driver.FindElement(By.XPath("(//div[@class='product-thumb'])[1]/div[2]/div[2]/button[1]")).Click();
+            Dictionary<string, string> Proddetails = reuse.GetProdDetails(1);
+            if (Proddetails["Check box 3"] == "Yes")
+                driver.FindElement(By.XPath("//div[@id='input-option223']/div[1]/label/input")).Click();
+            if (Proddetails["Check box 4"] == "Yes")
+                driver.FindElement(By.XPath("//div[@id='input-option223']/div[2]/label/input")).Click();
+            driver.FindElement(By.Id("input-option208")).Clear();
+            driver.FindElement(By.Id("input-option208")).SendKeys(Proddetails["Text"]);
+
+            driver.FindElement(By.Id("input-option217")).Click();
+            driver.FindElement(By.XPath("//option[contains(text(),'" + Proddetails["Color"] + "')]")).Click();
+
+            driver.FindElement(By.Id("input-option209")).SendKeys(Proddetails["Text area"]);
+            driver.FindElement(By.Id("button-upload222")).Click();
+
+            StringSelection strSelection = new StringSelection(Proddetails["File"]);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(strSelection, null);
+
+            Robot robot = new Robot();
+
+            robot.delay(300);
+
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+
+            Thread.Sleep(2000);
+
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+
+            robot.keyPress(KeyEvent.VK_SPACE);
+            robot.keyRelease(KeyEvent.VK_SPACE);
+            reuse.waitUntilElementLocated("//div[@class='text-danger']");
+
+
+            driver.FindElement(By.Id("input-option219")).Clear();
+            driver.FindElement(By.Id("input-option219")).SendKeys(Proddetails["Date"]);
+            driver.FindElement(By.Id("input-option221")).Clear();
+            driver.FindElement(By.Id("input-option221")).SendKeys(Proddetails["Time"]);
+            driver.FindElement(By.Id("input-option220")).Clear();
+            driver.FindElement(By.Id("input-option220")).SendKeys(Proddetails["Date & Time"]);
+            driver.FindElement(By.Id("input-quantity")).Clear();
+            driver.FindElement(By.Id("input-quantity")).SendKeys(Proddetails["Qty"]);
+            driver.FindElement(By.Id("button-cart")).Click();
+
+            //Further feature not working in application
+            reuse.logout();
+            reuse.writeResult("Logout : " + driver.FindElement(By.XPath("//div[@id='content']")).Text);
+        }
+
+        [TestMethod]
+        public void TC04_Validate_menus_and_count_links()
+        {
+            Dictionary<string, string> details = reuse.GetLogDetails(2);
+            driver.FindElement(By.XPath("//span[@class='caret']")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Login')]")).Click();
+            driver.FindElement(By.Id("input-email")).SendKeys(details["email"]);
+            driver.FindElement(By.Id("input-password")).SendKeys(details["password"]);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            reuse.takeScreenshot("Login");
+
+            reuse.writeResult("Number of menu links = " + driver.FindElements(By.XPath("//ul[@class='nav navbar-nav']/li")).Count);
+
+            IList<IWebElement> menu = driver.FindElements(By.XPath("//ul[@class='nav navbar-nav']/li/a"));
+            List<string> menuitems = new List<String>();
+            foreach (IWebElement elem in menu)
+            {
+                menuitems.Add(elem.Text);
+                Console.WriteLine( elem.Text);
+            }
+            foreach(string itm in menuitems)
+            {
+                if(driver.FindElements(By.XPath("//ul[@class='nav navbar-nav']/li/a[contains(text(),'" + itm + "')]")).Count>0)
+                    driver.FindElement(By.XPath("//ul[@class='nav navbar-nav']/li/a[contains(text(),'"+itm+"')]")).Click();
+                //  if (driver.FindElements(By.XPath("//ul[@class='nav navbar-nav']/li/a[contains(text(),'" + itm + "')]//parent::li/div/a[contains(text(),'Show All')]")).Count > 0)
+                try
+                {
+
+                    driver.FindElement(By.XPath("//ul[@class='nav navbar-nav']/li/a[contains(text(),'" + itm + "')]//parent::li/div/a[contains(text(),'Show All')]")).Click();
+                }
+                catch(Exception e)
+                {
+
+                }
+                    reuse.takeScreenshot("Menu_"+itm);
+            }
+            
+            reuse.logout();
+            reuse.writeResult("Logout : " + driver.FindElement(By.XPath("//div[@id='content']")).Text);
+
+        }
+
+        [TestMethod]
+        public void TC05_Navigate_through_Brands()
+        {
+
+            Dictionary<string, string> details = reuse.GetLogDetails(2);
+            driver.FindElement(By.XPath("//span[@class='caret']")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Login')]")).Click();
+            driver.FindElement(By.Id("input-email")).SendKeys(details["email"]);
+            driver.FindElement(By.Id("input-password")).SendKeys(details["password"]);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            reuse.takeScreenshot("Login");
+            driver.FindElement(By.LinkText("Brands")).Click();
+            IList<IWebElement> brands = driver.FindElements(By.XPath("//div[@id='content']/div/div/a"));
+            string brand;
+            for(int i = 0; i < brands.Count; i++)
+            {
+                brand = brands[i].Text;
+                brands[i].Click();
+                reuse.takeScreenshot("Brand_"+brand);
+                driver.FindElement(By.LinkText("Brands")).Click();
+                brands = driver.FindElements(By.XPath("//div[@id='content']/div/div/a"));
+            }
+
+            reuse.logout();
+            reuse.writeResult("Logout : " + driver.FindElement(By.XPath("//div[@id='content']")).Text);
+        }
+        }
+}
---
+++ components.cs
@@ -0,0 +1,213 @@
+﻿using GemBox.Spreadsheet;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace demo.opencart
+{
+    class components
+    {
+
+        IWebDriver driver;
+        WebDriverWait wait;
+        public components(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+        }
+
+     
+       public string readConfig(string key)
+        {
+            StreamReader sr = new StreamReader("C:\\Users\\Ashok\\source\\repos\\demo.opencart C#\\demo.opencart\\config.properties");
+
+            string word = "";
+            
+            while ((word=sr.ReadLine()) != null)
+            {
+                string Key = word.Split('=')[0];
+                string value = word.Split('=')[1];
+                if (Key == key)
+                    return value;
+            }
+            sr.Close();
+            return "";
+        }
+
+        public void LogOut()
+        {
+            driver.FindElement(By.XPath("//span[@class='caret']")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Logout')])[1]")).Click();
+        }
+
+        public Dictionary<string, string>  GetRegDetails(int id)
+        {
+            Dictionary<string, string> hash = new Dictionary<string, string>();
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            var workbook = ExcelFile.Load(@"C:\Users\Ashok\source\repos\demo.opencart C#\demo.opencart\Register.xlsx");
+            var worksheet = workbook.Worksheets[0];
+            var cell = worksheet.Cells[0, 0];
+            for(int i = 1; i < worksheet.Rows.Count; i++)
+            {
+                Console.WriteLine(worksheet.Cells[i, 0].Value);
+                if (worksheet.Cells[i, 0].Value.Equals(id))
+                {
+                    hash.Add("firstname", worksheet.Cells[i, 1].Value.ToString());
+                    hash.Add("lastname", worksheet.Cells[i, 2].Value.ToString());
+                    hash.Add("email", worksheet.Cells[i, 3].Value.ToString());
+                    hash.Add("phone", worksheet.Cells[i, 4].Value.ToString());
+                    hash.Add("password", worksheet.Cells[i, 5].Value.ToString());
+                    hash.Add("confirmpassword", worksheet.Cells[i, 6].Value.ToString());
+                    break;
+                }
+            }
+         
+            return hash;
+
+        }
+
+        public Dictionary<string, string> GetLogDetails(int id)
+        {
+            Dictionary<string, string> hash = new Dictionary<string, string>();
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            var workbook = ExcelFile.Load(@"C:\Users\Ashok\source\repos\demo.opencart C#\demo.opencart\Login.xlsx");
+            var worksheet = workbook.Worksheets[0];
+            var cell = worksheet.Cells[0, 0];
+            for (int i = 1; i < worksheet.Rows.Count; i++)
+            {
+                Console.WriteLine(worksheet.Cells[i, 0].Value);
+                if (worksheet.Cells[i, 0].Value.Equals(id))
+                {
+                    hash.Add("email", worksheet.Cells[i, 1].Value.ToString());
+                    hash.Add("password", worksheet.Cells[i, 2].Value.ToString());
+                    break;
+                }
+            }
+
+            return hash;
+
+        }
+
+        public Dictionary<string, string> GetProdDetails(int id)
+        {
+            Dictionary<string, string> hash = new Dictionary<string, string>();
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            var workbook = ExcelFile.Load(@"C:\Users\Ashok\source\repos\demo.opencart C#\demo.opencart\Product details.xlsx");
+            var worksheet = workbook.Worksheets[0];
+            var cell = worksheet.Cells[0, 0];
+            for (int i = 1; i < worksheet.Rows.Count; i++)
+            {
+                Console.WriteLine(worksheet.Cells[i, 0].Value);
+                if (worksheet.Cells[i, 0].Value.Equals(id))
+                {
+                    for(int j=1;j<11;j++)
+                        hash.Add(worksheet.Cells[0, j].Value.ToString(), worksheet.Cells[i, j].Value.ToString());
+                    break;
+                }
+            }
+
+            return hash;
+
+        }
+
+        public void writeResult(String text)
+        {
+            StreamWriter sr = new StreamWriter("C:\\Users\\Ashok\\source\\repos\\demo.opencart C#\\demo.opencart\\Result.txt",true);
+            sr.WriteLine(text);
+            sr.Close();
+        }
+
+        public void writeOutput(String text)
+        {
+            StreamWriter sr = new StreamWriter("C:\\Users\\Ashok\\source\\repos\\demo.opencart C#\\demo.opencart\\output data.txt", true);
+            sr.WriteLine(text);
+            sr.Close();
+        }
+
+        public void takeScreenshot(string name)
+        {
+
+            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+
+            screenshot.SaveAsFile("C:\\Users\\Ashok\\source\\repos\\demo.opencart C#\\demo.opencart\\Screenshot\\"+ DateTime.Now.ToString("MMddyyyy-HHmm") +"_"+ name+".jpeg", ScreenshotImageFormat.Jpeg);
+        }
+
+       
+
+        public void AddToCartRibbonClose()
+        {
+            Func<IWebDriver, bool> waitme = new Func<IWebDriver, bool>((IWebDriver driver) => {
+                //div[@class="alert alert-success alert-dismissible"]
+                if (driver.FindElement(By.XPath("//div[@class='alert alert-success alert-dismissible']")).Text.Contains("Success"))
+                    return true;
+                return false;
+            });
+            wait.Until(waitme);
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+            jse.ExecuteScript("arguments[0].scrollIntoView()", driver.FindElement(By.XPath("//button[@class='close']")));
+            Thread.Sleep(500);
+            driver.FindElement(By.XPath("//button[@class='close']")).Click();
+            
+        }
+
+        public void logout()
+        {
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a")).Click();
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+     
+
+        public void waitUntilElementLocated(string xpath)
+        {
+            Func<IWebDriver, bool> waitme = new Func<IWebDriver, bool>((IWebDriver driver) => {
+                if (driver.FindElements(By.XPath(xpath)).Count > 0)
+                    return true;
+                return false;
+            });
+            wait.Until(waitme);
+        }
+
+        public void waitUntilClickable(string xpath)
+        {
+            Func<IWebDriver, bool> waitme = new Func<IWebDriver, bool>((IWebDriver driver) => {
+                if (driver.FindElement(By.XPath(xpath)).Enabled && driver.FindElement(By.XPath(xpath)).Displayed)
+                    return true;
+                return false;
+            });
+            wait.Until(waitme);
+        }
+
+        public void scrollIntoView(string xpath)
+        {
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+            jse.ExecuteScript("arguments[0].scrollIntoView()", driver.FindElement(By.XPath(xpath)));
+        }
+
+        public void waitUntilTextLocated(string xpath,string text)
+        {
+            Func<IWebDriver, bool> waitme = new Func<IWebDriver, bool>((IWebDriver driver) => {
+                if (driver.FindElement(By.XPath(xpath)).Text.Contains(text))
+                    return true;
+                return false;
+            });
+            wait.Until(waitme);
+        }
+
+       
+
+     
+
+    }
+}
